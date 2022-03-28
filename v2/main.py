b05b320d2e3c066bdce1af67ec5788a6592e6386
joseporras00 <55441394+joseporras00@users.noreply.github.com>
2022-03-28 12:35:44 +0200

    Add files via upload

--- v2/main.py
+++ v2/main.py
@@ -41,27 +41,47 @@ cursor = con.cursor()
 
 #LEYENDO CSV DEL SERVIDOR FTP
 import ftplib
-ftp = ftplib.FTP("ftp.ipromor.com")
-ftp.login("iiot@ipromor.com", "9G6lYkGZ0W5w")
+def new_func():
+    ftp = ftplib.FTP("ftp.ipromor.com")
+    return ftp
+
+try:
+    ftp = new_func()
+    ftp.login("iiot@ipromor.com", "9G6lYkGZ0W5w")
+       # OK! conexión exitosa
+except Exception as e:
+    # Atrapar error
+    print("Ocurrió un error al conectar a servidor FTP: ", e)
 
 
+#Descargar archivo CSV
+try:
+    ftp.cwd('/')
+    ftp.retrbinary("RETR " + 'prueba.csv', open('prueba.csv', 'wb').write)
+    ftp.quit()
+   # OK! conexión exitosa
+except Exception as e:
+    # Atrapar error
+    print("Ocurrió un error al guardar csv en el equipo: ", e)
 
-ftp.cwd('/')
-ftp.retrbinary("RETR " + 'prueba.csv', open('prueba.csv', 'wb').write)
-ftp.quit()
-
+#Actualizar BD
 import csv
-with open('prueba.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
-    for row in reader:
+try:
+    with open('prueba.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
 
-        cursor.execute('SELECT * from ipromor WHERE idRegistro=?', row[0])
-        data=cursor.fetchone()
+            cursor.execute('SELECT * from ipromor WHERE idRegistro=?', row[0])
+            data=cursor.fetchone()
 
-        if data is None:
-            dato = [row[0],row[1],row[2],row[3],row[4],row[5],row[6]]
-            cursor.execute("INSERT INTO ipromor(cliente,maquina,fechahora,tipo,estado,descripcion) VALUES(?,?,?,?,?,?)",
-                        dato[1], dato[2], dato[3], dato[4], dato[5], dato[6])
-            con.commit()
+            if data is None:
+                dato = [row[0],row[1],row[2],row[3],row[4],row[5],row[6]]
+                cursor.execute("INSERT INTO ipromor(cliente,maquina,fechahora,tipo,estado,descripcion) VALUES(?,?,?,?,?,?)",
+                            dato[1], dato[2], dato[3], dato[4], dato[5], dato[6])
+                con.commit()
+   # OK! conexión exitosa
+except Exception as e:
+    # Atrapar error
+    print("Ocurrió un error al actualizar la BD: ", e)
+
 con.close()
-
